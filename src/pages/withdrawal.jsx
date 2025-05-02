fcc365ad090ebc6f78d055e3a07544fa7277336b
Gideon Kofi <kofi>
2025-05-02 12:27:46 +0100

    updating withrawal date

--- src/pages/withdrawal.jsx
+++ src/pages/withdrawal.jsx
@@ -130,8 +130,8 @@ const Withdrawal = () => {
   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
 </svg>
 
-          <h3>Withdrawals Begin Wednesday, July 31st, 2025</h3>
-<p>Our withdrawal system officially launches on <strong>Wednesday, July 31st, 2025</strong>. Until then, keep playing daily and grow your wallet. The more you earn now, the bigger your payout when it goes live!</p>
+          <h3>Withdrawals Begin on sartuday, May 31st, 2025</h3>
+<p>Our withdrawal system officially launches on <strong>sartuday, May 31st, 2025</strong>. Until then, keep playing daily and grow your wallet. The more you earn now, the bigger your payout when it goes live!</p>
 
             <button onClick={() => setShowModal(false)}>OK</button>
         </motion.div>
